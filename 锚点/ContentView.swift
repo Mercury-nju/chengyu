e48657fe096865d6af653822dc4d1e3be6649a5c
Daniel Lee <lihongyangnju@gmail.com>
2025-11-21 17:44:23 +0800

    Initial Commit

---
+++ 锚点/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  锚点
+//
+//  Created by Mercury on 2025/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 锚点/__App.swift
@@ -0,0 +1,17 @@
+//
+//  __App.swift
+//  锚点
+//
+//  Created by Mercury on 2025/11/21.
+//
+
+import SwiftUI
+
+@main
+struct __App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
